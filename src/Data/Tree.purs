525e624910891b399d0b1e8c3a3d8bbe99ce588c
Matthieu Bulté <matthieu.bulte.06@gmail.com>
2015-09-17 22:08:25 +0200

    Add phantom types to switch algorithm used to traverse the tree

--- src/Data/Tree.purs
+++ src/Data/Tree.purs
@@ -1,5 +1,7 @@
 module Data.Tree
   ( Tree(..)
+  , DepthFirst()
+  , BreadthFirst()
   , flatten
   ) where
 
@@ -9,39 +11,47 @@ import Data.Foldable (Foldable, foldr, foldl, foldMap)
 import Data.Traversable (Traversable, traverse, sequence)
 import Data.NonEmpty (NonEmpty(), (:|))
 
-data Tree a = Tree a (Array (Tree a))
+data DepthFirst
+data BreadthFirst
 
-instance showTree :: (Show a) => Show (Tree a) where
+data Tree alg a = Tree a (Array (Tree alg a))
+
+instance showTree :: (Show a) => Show (Tree alg a) where
   show (Tree a ts) = "Tree " <> show a <> " " <> show ts
 
-instance eqTree :: (Eq a) => Eq (Tree a) where
+instance eqTree :: (Eq a) => Eq (Tree alg a) where
   eq (Tree a ts) (Tree a' ts') = a == a' && ts == ts'
 
-instance functorTree :: Functor Tree where
+instance functorTree :: Functor (Tree alg) where
   map f (Tree a ts) = Tree (f a) (map f <$> ts)
 
-instance applyTree :: Apply Tree where
+instance applyTree :: Apply (Tree alg) where
   apply (Tree f tf) t@(Tree a ta) = Tree (f a) (map (f <$>) ta <> map (<*> t) tf)
 
-instance applicativeTree :: Applicative Tree where
+instance applicativeTree :: Applicative (Tree alg) where
   pure a = Tree a []
 
-instance bindTree :: Bind Tree where
+instance bindTree :: Bind (Tree alg) where
   bind (Tree a ts) f = case f a of
     Tree a' ts' -> Tree a' (ts' <> map (`bind` f) ts)
 
-instance monadTree :: Monad Tree
+instance monadTreeDepthFirst :: Monad (Tree DepthFirst)
+instance monadTreeBreadthFirst :: Monad (Tree BreadthFirst)
 
-instance foldableTree :: Foldable Tree where
+instance foldableTree :: Foldable (Tree alg) where
   foldr f b (Tree a ts) = f a (foldr (flip $ foldr f) b ts)
   foldl f b (Tree a ts) = f (foldl (foldl f) b ts) a
   foldMap f (Tree a ts) = f a <> foldMap (foldMap f) ts
 
-instance traversableTree :: Traversable Tree where
+instance traversableTreeDepthFirst :: Traversable (Tree DepthFirst) where
   traverse f (Tree a ts) = Tree <$> f a <*> traverse (traverse f) ts
   sequence = traverse id
 
-flatten :: forall a. Tree a -> NonEmpty Array a
+instance traversableTreeBreadthFirst :: Traversable (Tree BreadthFirst) where
+  traverse f (Tree a ts) = (flip Tree) <$> traverse (traverse f) ts <*> f a
+  sequence = traverse id
+
+flatten :: forall alg a. Tree alg a -> NonEmpty Array a
 flatten (Tree a ts) = a :| concatMap flatten' ts
   where flatten' (Tree a []) = [a]
         flatten' (Tree a ts) = a : concatMap flatten' ts
