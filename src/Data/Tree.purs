8ba3815468c2f6d29b8fd1175964a5b92a84d772
Matthieu Bulté <matthieu.bulte.06@gmail.com>
2015-09-17 21:46:12 +0200

    Unify variable names

--- src/Data/Tree.purs
+++ src/Data/Tree.purs
@@ -15,20 +15,20 @@ instance showTree :: (Show a) => Show (Tree a) where
   show (Tree a ts) = "Tree " <> show a <> " " <> show ts
 
 instance eqTree :: (Eq a) => Eq (Tree a) where
-  eq (Tree a aTs) (Tree b bTs) = a == b && aTs == bTs
+  eq (Tree a ts) (Tree a' ts') = a == a' && ts == ts'
 
 instance functorTree :: Functor Tree where
   map f (Tree a ts) = Tree (f a) (map f <$> ts)
 
 instance applyTree :: Apply Tree where
-  apply (Tree f fTs) aT@(Tree a aTs) = Tree (f a) (map (f <$>) aTs <> map (<*> aT) fTs)
+  apply (Tree f tf) t@(Tree a ta) = Tree (f a) (map (f <$>) ta <> map (<*> t) tf)
 
 instance applicativeTree :: Applicative Tree where
   pure a = Tree a []
 
 instance bindTree :: Bind Tree where
-  bind (Tree x ts) f = case f x of
-    Tree x' ts' -> Tree x' (ts' <> map (`bind` f) ts)
+  bind (Tree a ts) f = case f a of
+    Tree a' ts' -> Tree a' (ts' <> map (`bind` f) ts)
 
 instance monadTree :: Monad Tree
 
@@ -38,7 +38,7 @@ instance foldableTree :: Foldable Tree where
   foldMap f (Tree a ts) = f a <> foldMap (foldMap f) ts
 
 instance traversableTree :: Traversable Tree where
-  traverse f (Tree x ts) = Tree <$> f x <*> traverse (traverse f) ts
+  traverse f (Tree a ts) = Tree <$> f a <*> traverse (traverse f) ts
   sequence = traverse id
 
 flatten :: forall a. Tree a -> NonEmpty Array a
