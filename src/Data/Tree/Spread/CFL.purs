3b352e0610545f966c7098956482d34b61370b0c
Matt Parsons <parsonsmatt@gmail.com>
2016-04-09 15:57:44 -0400

    style, warnings

--- src/Data/Tree/Spread/CFL.purs
+++ src/Data/Tree/Spread/CFL.purs
@@ -3,17 +3,15 @@ module Data.Tree.Spread.CFL where
 
 import Prelude
 
-import Control.Bind
-import Control.Comonad.Cofree (Cofree, mkCofree)
-import Control.Comonad.Cofree as CF
-import Data.List.Lazy
-import Math as Math
-import Control.Extend (class Extend, extend)
-import Data.Traversable (class Traversable, traverse, sequence)
-import Data.Lazy
 import Control.Comonad (class Comonad, extract)
+import Control.Comonad.Cofree as CF
+import Control.Comonad.Cofree (Cofree, mkCofree)
+import Control.Extend (class Extend, extend)
 import Data.Foldable (class Foldable, foldr, foldl, foldMap, and)
 import Data.Int as Int
+import Data.List.Lazy (List, (:), nil)
+import Data.Traversable (class Traversable, traverse)
+import Math as Math
 
 import Test.QuickCheck.Arbitrary (class Coarbitrary, class Arbitrary, coarbitrary, arbitrary)
 import Test.QuickCheck.Gen (sized)
