c21589e275f60ea5c03a4d53dac0e335c09e924c
rintcius <rintcius@gmail.com>
2016-12-22 12:48:16 +0100

    upgrade to purescript 0.10

--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -1,6 +1,12 @@
 module Test.Main where
 
 import Test.Data.Tree
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE)
+import Control.Monad.Eff.Exception (EXCEPTION)
+import Control.Monad.Eff.Random (RANDOM)
+import Prelude (Unit)
 
+main :: forall eff. Eff( console :: CONSOLE, random :: RANDOM, err :: EXCEPTION | eff) Unit
 main = do
   testTree
